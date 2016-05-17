badbc6965c9f143b4c660a4fd1975a1caa43d859
Justin Jia <jibin.jia@demandforce.com>
2016-05-17 22:39:47 +0800

    Move the function of org root prepare to middleware orgsUtility.

---
+++ middleware/orgsUtility.js
@@ -0,0 +1,33 @@
+﻿var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
+
+var orgRoot = path.join(process.env.PROGRAMDATA, 'pearl', 'public', 'orgconfig');
+var templatePath = path.join(orgRoot, 'template.json');
+
+function prepareOrgRoot(callback) {
+    mkdirp(orgRoot, function (err) {
+        if (err) {
+            console.log('Failed to prepare org root, due to: ' + err);
+        } else {
+            var templatePath = path.join(orgRoot, 'template.json');
+            fs.exists(templatePath, function (exists) {
+                if (exists == false) {
+                    fs.writeFile(templatePath, '{\"configuration\":null,\"system_information\":null,\"deleted\":false}', function (err) {
+                        if (err) {
+                            console.log('Failed to generate org template, due to: ' + err);
+                            callback(false);
+                        } else {
+                            callback(true);
+                        }
+                    });
+                } else {
+                    callback(true);
+                }
+            });
+        }
+    });
+}
+
+exports.prepareOrgRoot = prepareOrgRoot;
+
