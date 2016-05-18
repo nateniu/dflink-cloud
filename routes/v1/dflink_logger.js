0098d657fa55bee81e3003032a90b2ceacfbc329
Nate Niu <nate.niu@internetbrands.com>
2016-05-18 00:46:47 -0700

    fix logger not defined

--- routes/v1/dflink_logger.js
+++ routes/v1/dflink_logger.js
@@ -1,20 +1,25 @@
-/**
- * Created by INTGDev on 5/18/2016.
- */
+
 var express = require('express');
 var router = express.Router();
 var path = require('path');
 var global = require('../../utils/global.js');
+var logger = require('../../utils/logger');
 var rootPath = path.join(global.DBROOT, 'public');
 
 var fs = require('fs');
 
 var splunk_path = rootPath + '/posttosplunk.txt';
 
-router.post('/', function(req, res){
-  fs.appendFile(splunk_path, JSON.stringify(req.body) + '\r\n', 'utf-8' , function(err){
-    logger.error(err);
-  });
-})
+router.post('/', function(req, res) {
+    fs.appendFile(splunk_path, JSON.stringify(req.body) + '\r\n', 'utf-8', function (err) {
+        if (err) {
+            logger.error(err);
+            res.status(500);
+        } else {
+            res.status(200);
+        }
+        res.end();
+    });
+});
 
 module.exports = router;
