a7a29431371d80576d23b54de199e7582ff0e368
Nate Niu <nate.niu@demandforce.com>
2017-03-15 17:30:27 +0800

    Fix 2way post action

--- routes/v1/2way.js
+++ routes/v1/2way.js
@@ -1,8 +1,11 @@
 ﻿var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
 
 /* GET mock appointment list */
 router.get('/confirmationlist', function (req, res) { 
+    console.log(req.query);
    var nameindex = req.query.username.indexOf('@');
    var name = req.query.username.substring(0, nameindex);
    res.redirect('../../../confirmationlist/'.concat(name).concat('_appts.xml'));
@@ -12,7 +15,14 @@ router.get('/confirmationlist', function (req, res) {
 router.post('/confirmationlist', function (req, res) { 
    var nameindex = req.query.username.indexOf('@');
    var name = req.query.username.substring(0, nameindex);
-   res.redirect('../../../confirmationlist//'.concat(name).concat('_appts.xml'));
-});
+   res.set('Content-Type', 'text/xml');
+   var rootpath = path.dirname(require.main.filename || process.mainModule.filename);
+   var xmlfile =  path.join(rootpath, '..','public', 'confirmationlist', name.concat('_appts.xml'));
+   console.log(xmlfile);
+   fs.readFile(xmlfile, function(err, data) {
+    res.send(data);
+   });
+ });
+
 
 module.exports = router;
